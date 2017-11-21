4651a7506ceb1c1dc67565c5edf2affb6d8c545d
Arcnet <bcscarney@gmail.com>
2017-11-21 17:04:39 -0600

    basic touch input testing

--- core/src/com/jump/game/JumpGame.java
+++ core/src/com/jump/game/JumpGame.java
@@ -5,34 +5,40 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import java.util.Map;
+import java.util.HashMap;
 
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class JumpGame extends ApplicationAdapter implements InputProcessor{
 	private Viewport viewport;
 	private Camera camera;
 
 	private BitmapFont font;
-    private SpriteBatch batch;
-	private String message = "touch something";
+	private String message = "Touch something already!";
+	private SpriteBatch batch;
 	private int w,h;
 
+	class TouchInfo {
+		public float touchX = 0;
+		public float touchY = 0;
+		public boolean touched = false;
+	}
 
-	class TouchInfo{
-	    public float touchX = 0;
-	    public float touchY = 0;
-	    public boolean touched = false;
-    }
-    private Map<Integer,TouchInfo> touches = new HashMap<Integer,TouchInfo>();
+	private Map<Integer,TouchInfo> touches = new HashMap<Integer,TouchInfo>();
 
 	@Override
 	public void create () {
@@ -43,33 +49,38 @@ public class JumpGame extends ApplicationAdapter implements InputProcessor{
 		float screenHeight = Gdx.graphics.getHeight();
 		float screenWidth = Gdx.graphics.getWidth();
 
-		batch = new SpriteBatch();
 		font = new BitmapFont();
 		font.setColor(Color.RED);
-		Gdx.input.setInputProcessor(this);
-		for(int i=0;i<5;i++){
-		    touches.put(i,new TouchInfo());
-        }
-        w = Gdx.graphics.getWidth();
+		batch = new SpriteBatch();
+		w = Gdx.graphics.getWidth();
 		h = Gdx.graphics.getHeight();
+		Gdx.input.setInputProcessor(this);
+		for(int i = 0; i < 5; i++){
+			touches.put(i, new TouchInfo());
+		}
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 1, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
+
 		message = "";
-		for(int i=0;i<5;i++){
-		    if(touches.get(i).touched){
-		        message+= "Finger:" + Integer.toString(i) + "touch at:" +
-                        Float.toString(touches.get(i).touchX) + "," +
-                        Float.toString(touches.get(i).touchY) + "\n";
-            }
-        }
-        float x = w/2;
+		for(int i = 0; i < 5; i++){
+			if(touches.get(i).touched)
+				message += "Finger:" + Integer.toString(i) + "touch at:" +
+						Float.toString(touches.get(i).touchX) +
+						"," +
+						Float.toString(touches.get(i).touchY) +
+						"\n";
+
+		}
+		float x = w/2;
 		float y = h/2;
-		font.draw(batch, message, x, y);
+		font.draw(batch, message, touches.get(0).touchX, touches.get(0).touchY);
+
 		batch.end();
 	}
 	
@@ -78,6 +89,7 @@ public class JumpGame extends ApplicationAdapter implements InputProcessor{
 		batch.dispose();
 		font.dispose();
 	}
+
 	@Override
 	public boolean keyDown(int keycode) {
 		return false;
