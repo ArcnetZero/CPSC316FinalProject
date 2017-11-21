83d510dce2eeaedb19a709ebc85712a7d776899d
Arcnet <bcscarney@gmail.com>
2017-11-21 16:00:50 -0600

    basic touch input testing

--- core/src/com/jump/game/JumpGame.java
+++ core/src/com/jump/game/JumpGame.java
@@ -2,14 +2,11 @@ package com.jump.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.input.GestureDetector;
-import com.badlogic.gdx.input.GestureDetector.GestureListener;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 import com.badlogic.gdx.graphics.Camera;
@@ -20,7 +17,7 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 import java.util.HashMap;
 import java.util.Map;
 
-public class JumpGame extends ApplicationAdapter {
+public class JumpGame extends ApplicationAdapter implements InputProcessor{
 	private Viewport viewport;
 	private Camera camera;
 
@@ -28,6 +25,8 @@ public class JumpGame extends ApplicationAdapter {
     private SpriteBatch batch;
 	private String message = "touch something";
 	private int w,h;
+
+
 	class TouchInfo{
 	    public float touchX = 0;
 	    public float touchY = 0;
@@ -45,7 +44,7 @@ public class JumpGame extends ApplicationAdapter {
 		float screenWidth = Gdx.graphics.getWidth();
 
 		batch = new SpriteBatch();
-		font = new BitmapFont(Gdx.files.internal("data/arial-15.fnt"),false);
+		font = new BitmapFont();
 		font.setColor(Color.RED);
 		Gdx.input.setInputProcessor(this);
 		for(int i=0;i<5;i++){
@@ -70,9 +69,7 @@ public class JumpGame extends ApplicationAdapter {
         }
         float x = w/2;
 		float y = h/2;
-		GlyphLayout layout = new GlyphLayout();
-		layout.setText(message);
-        font.draw(batch, message, x, y);
+		font.draw(batch, message, x, y);
 		batch.end();
 	}
 	
@@ -80,5 +77,54 @@ public class JumpGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		font.dispose();
+	}
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if(pointer < 5){
+			touches.get(pointer).touchX = screenX;
+			touches.get(pointer).touchY = screenY;
+			touches.get(pointer).touched = true;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		if(pointer < 5){
+			touches.get(pointer).touchX = 0;
+			touches.get(pointer).touchY = 0;
+			touches.get(pointer).touched = false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
